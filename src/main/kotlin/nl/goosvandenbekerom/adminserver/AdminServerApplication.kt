824d64e1fc3158d79361ebd491c03c40ddfbc478
Goos van den Bekerom <goos.bekerom@gmail.com>
2019-05-08 23:08:37 +0200

    Initial commit, simple spring boot admin server setup

---
+++ src/main/kotlin/nl/goosvandenbekerom/adminserver/AdminServerApplication.kt
@@ -0,0 +1,13 @@
+package nl.goosvandenbekerom.adminserver
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableAdminServer
+class AdminServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdminServerApplication>(*args)
+}
